a903635efc0fc42565bf001b96d733b319ae403d
marwane2001 <marwanebenhadj@gmail.com>
2024-07-23 12:55:33 +0100

    added features

---
+++ src/Twig/AppExtensions.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Twig;
+use App\Repository\CategoryRepository;
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
+
+class AppExtensions extends AbstractExtension implements GlobalsInterface  {
+    private $categoryRepository;
+    public function __construct(CategoryRepository $categoryRepository) {
+        $this->categoryRepository = $categoryRepository;
+
+    }
+    public function getFilters() {
+
+        return [
+          new TwigFilter('price', [$this, 'formatPrice']),
+        ];
+    }
+    public function formatPrice($number) {
+
+        return number_format($number,'2',',').'$';
+    }
+
+    public function getGlobals():array {
+        return [
+                'allcategories' => $this->categoryRepository->findAll(),
+
+        ];
+
+    }
+
+}
