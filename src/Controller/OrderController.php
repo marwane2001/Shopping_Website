0afa94148ba15cbc5694eb9da5a007bad27f3f39
marwane2001 <marwanebenhadj@gmail.com>
2024-07-26 13:32:59 +0100

    Shipping part updates

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -3,15 +3,14 @@
 namespace App\Controller;
 
 use App\Form\OrderType;
+use App\Class\Cart;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\User;
 
-class OrderController extends AbstractController
-{
-
-
+class OrderController extends AbstractController{
     #[Route('/order/shipping', name: 'app_order')]
     public function index(): Response
     {
@@ -28,10 +27,31 @@ class OrderController extends AbstractController
         }
 
         $form = $this->createForm(OrderType::class, null, [
-            'addresses' => $addresses
+            'addresses' => $addresses,
+            'action' => $this->generateUrl('app_order_summary')
         ]);
         return $this->render('order/index.html.twig', [
             'deliveryform' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/order/summary', name: 'app_order_summary')]
+    public function add(Request $request,Cart $cart): Response
+    {
+
+        $form=$this->createForm(OrderType::class,null,[
+            'addresses' => $this->getUser()->getAdresses(),
+        ]);
+
+        $form->handleRequest($request);
+//        if ($form->isSubmitted() && $form->isValid()) {
+//
+//        }
+
+        return $this->render('order/summary.html.twig', [
+            'choices'=>$form->getData(),
+            'cart'=>$cart->getCart(),
+            'totalWt'=>$cart->getTotalWt(),
         ]);
     }
 }
