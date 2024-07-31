d88be2a97463f92adab2af81fa6af1d7a28ec64d
marwane2001 <marwanebenhadj@gmail.com>
2024-07-31 17:53:42 +0100

    fixed some code in the payment part#2

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -7,64 +7,63 @@ use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
-    #[Route('/cart', name: 'app_cart')]
-    public function index(Cart $cart): Response
+    #[Route('/cart/{pattern}', name: 'app_cart', defaults: ['pattern' => null])]
+    public function index(Cart $cart, ?string $pattern): Response
     {
+        if ($pattern === "cancel") {
+            $this->addFlash(
+                'info',
+                'Payment Canceled: You can update your Cart and Order'
+            );
+        }
 
         return $this->render('cart/cart.html.twig', [
             'cart' => $cart->getCart(),
-            'totalWt'=>$cart->getTotalWt(),
-
+            'totalWt' => $cart->getTotalWt(),
         ]);
     }
 
-
     #[Route('/cart/add/{id}', name: 'app_cart_add')]
-    public function add($id,Cart $cart,ProductRepository $productRepository,Request $request): Response
+    public function add(int $id, Cart $cart, ProductRepository $productRepository, Request $request): Response
     {
+        $product = $productRepository->find($id);
+        if ($product) {
+            $cart->add($product);
+            $this->addFlash(
+                'success', "Product has been added to cart successfully!"
+            );
+        } else {
+            $this->addFlash(
+                'error', "Product not found!"
+            );
+        }
 
-        $product = $productRepository->findOneById($id);
-        $cart->add($product);
-        $this->addFlash(
-            'success',"Product has been added to cart successfully!"
-        );
-        return $this->redirect( $request->headers->get('referer'));
-
-
-
+        return $this->redirect($request->headers->get('referer'));
     }
 
     #[Route('/cart/decrease/{id}', name: 'app_cart_decrease')]
-    public function decrease($id,Cart $cart ): Response
+    public function decrease(int $id, Cart $cart): Response
     {
-
-
         $cart->decrease($id);
         $this->addFlash(
-            'success',"Product has Deleted from the cart successfully!"
+            'success', "Product has been removed from the cart successfully!"
         );
 
         return $this->redirectToRoute('app_cart');
-
     }
-
 
     #[Route('/cart/remove', name: 'app_cart_remove')]
     public function remove(Cart $cart): Response
     {
-
-
-         $cart->remove();
+        $cart->remove();
         $this->addFlash(
-            'success',"the cart has been cleared successfully!"
+            'success', "The cart has been cleared successfully!"
         );
 
-        return $this->redirectToRoute('app_home');
-
+        return $this->redirectToRoute('app_login');
     }
-
 }
--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -4,24 +4,25 @@ namespace App\Controller;
 use App\Repository\OrderRepository;
 use Stripe\Stripe;
 use Stripe\Checkout\Session;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class PaymentController extends AbstractController
 {
-    
+
 
 
     #[Route('/order/payment/{id_order}', name: 'app_payment')]
-    public function index($id_order,OrderRepository $orderRepository): Response
+    public function index($id_order,OrderRepository $orderRepository,EntityManagerInterface $entityManager): Response
     {
         $YOUR_DOMAIN = $_ENV['DOMAIN'];
         $stripeApiKey = $_ENV['STRIPE_API_KEY'];
         Stripe::setApiKey($stripeApiKey);
         $order=$orderRepository->findOneById($id_order);
 
-   
+
         if(!$order){
             return $this->redirectToRoute('app_home');
         }
@@ -41,17 +42,17 @@ class PaymentController extends AbstractController
                             'images' => [
                                 $YOUR_DOMAIN.'/upload-dir/'.$product->getProdcutIllustration()
                             ]
-                          
+
                         ]
                     ] ,
                     'quantity' => $product->getProductQuantity(),
 
 
             ];
-           
-            
+
+
         }
-    
+
         $product_for_stripe[]=[
 
             'price_data' =>[
@@ -77,11 +78,37 @@ class PaymentController extends AbstractController
                     $product_for_stripe
             ]],
             'mode' => 'payment',
-            'success_url' => $YOUR_DOMAIN . '/success.html',
-            'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+            'success_url' => $YOUR_DOMAIN . '/order/thanks/{CHECKOUT_SESSION_ID}',
+            'cancel_url' => $YOUR_DOMAIN . '/cart/cancel',
         ]);
         header("HTTP/1.1 301 See Other");
+        $order->setStripeSessionId($checkout_session->id);
+        $entityManager->flush();
+
+
         return $this->redirect($checkout_session->url);
 
+    }
+
+        #[Route('/order/thanks/{stripe_session_id}', name: 'app_payment_success')]
+    public function success($stripe_session_id,OrderRepository $orderRepository,EntityManagerInterface $entityManager): Response
+    {
+        $order=$orderRepository->findOneBy([
+            'stripe_session_id'=>$stripe_session_id,
+            'user'=>$this->getUser()
+
+        ]);
+        if(!$order){
+            return $this->redirectToRoute('app_home');
+        }
+        if ($order->getState()==1){
+            $order->setState(2);
+            $entityManager->flush();
+        }
+
+        return $this->render('payment/success.html.twig', [
+            'order'=>$order,
+
+        ]);
     }
 }
